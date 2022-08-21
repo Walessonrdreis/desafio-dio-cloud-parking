2c7d2dc03f3225ce77b9c3ae063c77b08b9c4fa6
Walessonrdreis <wallllase@hotmail.com>
2022-08-21 18:37:45 -0300

    update: pacote config

--- src/main/java/one/digitalinnovation/cloudparking/config/SwaggerConfig.java
+++ src/main/java/one/digitalinnovation/cloudparking/config/SwaggerConfig.java
@@ -2,8 +2,11 @@ package one.digitalinnovation.cloudparking.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
