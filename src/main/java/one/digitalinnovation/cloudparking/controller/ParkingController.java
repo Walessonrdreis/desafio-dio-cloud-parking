3347efc3af8d0e91a13eac69d519b28f2a0815d9
Walessonrdreis <wallllase@hotmail.com>
2022-08-21 18:35:00 -0300

    update: ParkingController, ParkingMapper, SwaggerConfig, pom.xml

--- src/main/java/one/digitalinnovation/cloudparking/controller/ParkingController.java
+++ src/main/java/one/digitalinnovation/cloudparking/controller/ParkingController.java
@@ -1,9 +1,12 @@
 package one.digitalinnovation.cloudparking.controller;
 
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/parking")
+@Api
 public class ParkingController {
 
 
